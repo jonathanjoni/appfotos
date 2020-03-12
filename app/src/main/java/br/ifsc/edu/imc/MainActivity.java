cb207437d4a8b354209e84430d4cfb3ef11e8464
Computador ifsc <ifsc@ifsc.com.br>
2020-03-12 11:14:44 -0300

    pronto

--- app/src/main/java/br/ifsc/edu/imc/MainActivity.java
+++ app/src/main/java/br/ifsc/edu/imc/MainActivity.java
@@ -6,17 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    List<Integer> lista_imagens;
+    int posicaoImagem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
+
+        lista_imagens = Arrays.asList(
+
+                R.drawable.cachorro,
+                R.drawable.gardem,
+                R.drawable.happy);
+        posicaoImagem = 0;
     }
-    public void mudatexto (View view) {
-        imageView.setImageResource(R.drawable.cachorro);
+
+
+    public void mudaFoto(View view) {
+
+        if (posicaoImagem < lista_imagens.size() - 1) {
+            imageView.setImageResource(lista_imagens.get(posicaoImagem));
+        } else {
+            posicaoImagem = 0;
+            imageView.setImageResource(lista_imagens.get(posicaoImagem));
+        }
+        posicacoimagem++;
     }
 
 
